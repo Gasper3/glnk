d5ced1e1e5459c2e9f1192638ed62a28aff246ef
Gasper3 <trzecik65@gmail.com>
2021-12-02 22:10:58 +0100

    Views refactor and added index view

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,19 @@
-from django.http.response import JsonResponse
-from django.shortcuts import get_object_or_404
-from rest_framework import generics, mixins
+from django.http import response
+from rest_framework import generics
 
 from .models import Url
 from .serializers import UrlSerializer
 
 
-class RetrieveUrl(mixins.RetrieveModelMixin, generics.GenericAPIView):
+def index(request):
+    return response.HttpResponse()
+
+
+class RetrieveUrl(generics.RetrieveAPIView):
     serializer_class = UrlSerializer
     queryset = Url.objects.all()
+    lookup_field = 'short_url'
 
-    def retrieve(self, request, short_url=None):
-        queryset = Url.objects.all()
-        url = get_object_or_404(queryset, short_url=short_url)
-
-        serializer = UrlSerializer(url)
-
-        return JsonResponse(serializer.data)
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+class CreateShortUrl(generics.CreateAPIView):
+    serializer_class = UrlSerializer
+    queryset = Url.objects.all()

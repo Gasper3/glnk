f2d459921b04f3ab4fc0df0c29f11ad387e57dd3
Gasper3 <trzecik65@gmail.com>
2021-12-02 22:27:57 +0100

    Created setting to determine size of shorted url

--- main_app/managers.py
+++ main_app/managers.py
@@ -1,9 +1,10 @@
 from django.db import models
 import string
 import random
+from django.conf import settings
 
 class UrlManager(models.Manager):
     def generate_short_url(self):
         letters = string.ascii_letters
-        short_url = ''.join(random.choice(letters) for i in range(5))
+        short_url = ''.join(random.choice(letters) for i in range(settings.SHORT_URL_SIZE))
         return short_url

48d1868f19aa37b432e364f670eabe4c52653f1b
Gasper3 <trzecik65@gmail.com>
2021-12-02 22:19:35 +0100

    Handling url creation

--- main_app/serializers.py
+++ main_app/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 from .models import Url
 
 class UrlSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
+    id = serializers.ReadOnlyField()
     url = serializers.CharField()
-    short_url = serializers.CharField()
+    short_url = serializers.ReadOnlyField()
+
+    def create(self, validated_data: dict):
+        url = Url()
+        url.url = validated_data.get("url")
+        url.short_url = Url.objects.generate_short_url()
+        url.save()
+
+        return url

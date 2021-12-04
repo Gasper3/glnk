369755e2dddb78082b709c55a31609b9a66ec211
Gasper3 <trzecik65@gmail.com>
2021-12-04 11:11:04 +0100

    Added names to urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from .views import CreateShortUrl, RetrieveUrl, index
 
 urlpatterns = [
     path("", index),
-    path("url", CreateShortUrl.as_view()),
-    path("<str:short_url>", RetrieveUrl.as_view()),
+    path("url", CreateShortUrl.as_view(), name="create-url"),
+    path("<str:short_url>", RetrieveUrl.as_view(), name="get-url"),
 ]

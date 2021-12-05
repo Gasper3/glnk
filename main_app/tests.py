e22031a159ebcf65cc13f2b91dbf3eff4f2d1f93
Gasper3 <trzecik65@gmail.com>
2021-12-05 14:36:11 +0100

    Corrected tests after changes

--- main_app/tests.py
+++ main_app/tests.py
@@ -9,8 +9,8 @@ from .models import Url
 
 class UrlRESTTestCase(APITestCase):
     def setUp(self) -> None:
-        Url.objects.create(url="wp.pl", short_url="qWeRt")
-        Url.objects.create(url="google.com", short_url="asdFGH")
+        Url.objects.create(url="https://wp.pl", short_url="qWeRt")
+        Url.objects.create(url="https://google.com", short_url="asdFGH")
 
     def test_get_url(self):
         url = reverse("get-url", kwargs={"short_url": "qWeRt"})
@@ -18,7 +18,7 @@ class UrlRESTTestCase(APITestCase):
         data: dict = response.data
 
         self.assertEqual(status.HTTP_200_OK, response.status_code)
-        self.assertEqual("wp.pl", data.get("url"))
+        self.assertEqual("https://wp.pl", data.get("url"))
 
     def test_get_url_404(self):
         url = reverse("get-url", kwargs={"short_url": "not_exist"})
@@ -28,27 +28,27 @@ class UrlRESTTestCase(APITestCase):
 
     def test_create_url(self):
         url = reverse("create-url")
-        response = self.client.post(url, {"url": "onet.pl"})
+        response = self.client.post(url, {"url": "https://onet.pl"})
         data: dict = response.data
 
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
-        self.assertEqual(data.get("url"), "onet.pl")
+        self.assertEqual("https://onet.pl", data.get("url"))
         self.assertIsNotNone(data.get("short_url"))
         self.assertNotEqual(data.get("short_url"), "")
 
     def test_create_url_get_existing(self):
         url = reverse("create-url")
-        response = self.client.post(url, {"url": "google.com"})
+        response = self.client.post(url, {"url": "https://google.com"})
         data: dict = response.data
 
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
-        self.assertEqual("google.com", data.get("url"))
+        self.assertEqual("https://google.com", data.get("url"))
         self.assertEqual("asdFGH", data.get("short_url"))
 
     def test_create_url_check_ip_and_user_agent(self):
         url = reverse("create-url")
-        response = self.client.post(url, {"url": "asd.pl"}, HTTP_USER_AGENT="ApiClientTestCase")
-        created_url: Url = Url.objects.get(url="asd.pl")
+        response = self.client.post(url, {"url": "http://asd.pl"}, HTTP_USER_AGENT="ApiClientTestCase")
+        created_url: Url = Url.objects.get(url="http://asd.pl")
 
         self.assertEqual(status.HTTP_201_CREATED, response.status_code)
         self.assertEqual("127.0.0.1", created_url.ip_address)

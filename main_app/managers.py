2c73f767e311d02a2dccab0d554d08013b18a004
Gasper3 <trzecik65@gmail.com>
2021-12-05 21:31:26 +0100

    Generating short url if it already exists

--- main_app/managers.py
+++ main_app/managers.py
@@ -9,4 +9,6 @@ class UrlManager(models.Manager):
     def generate_short_url(self):
         letters = string.ascii_letters
         short_url = ''.join(random.choice(letters) for i in range(settings.SHORT_URL_SIZE))
+        while self.filter(short_url=short_url):
+            short_url = ''.join(random.choice(letters) for i in range(settings.SHORT_URL_SIZE))
         return short_url

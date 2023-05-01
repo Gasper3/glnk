3b20b10fa5fda9831d85dc630d0b0c5c21fbf32d
Gasper3 <trzecik65@gmail.com>
2023-05-01 23:00:31 +0200

    Improve docs

--- app/routers/url_router.py
+++ app/routers/url_router.py
@@ -4,14 +4,16 @@ from fastapi.routing import APIRouter
 from ..db import db_session
 from ..dependencies import UrlDep
 from ..repositories import UrlRepository, UrlVisitsRepository
-from ..schemas import UrlRequest
-from ..utils import generate_short_url
+from .. import schemas
+from ..utils import generate_short_url, common_responses, get_responses
 
 api_router = APIRouter(prefix='/api/url')
 
 
-@api_router.post('', status_code=status.HTTP_201_CREATED)
-async def create_short_url(body: UrlRequest, request: Request):
+@api_router.post(
+    '', status_code=status.HTTP_201_CREATED, response_model=schemas.ShortUrlResponse, responses={**common_responses}
+)
+async def create_short_url(body: schemas.UrlRequest, request: Request):
     with db_session() as session:
         repository = UrlRepository(session)
         url_obj, created = repository.get_or_create(url=body.url)
@@ -22,21 +24,15 @@ async def create_short_url(body: UrlRequest, request: Request):
             session.flush()
         session.expunge(url_obj)
 
-    return {
-        'short_url': url_obj.short_url,
-    }
+    return {'short_url': url_obj.short_url}
 
 
-@api_router.get('/{short_url}')
+@api_router.get('/{short_url}', responses={**get_responses}, response_model=schemas.UrlResponse)
 async def get_url(request: Request, url_obj: UrlDep):
     with db_session() as session:
         UrlVisitsRepository(session).create(
-            url=url_obj,
-            user_agent=request.headers.get('user-agent'),
-            ip_address=request.client.host
+            url=url_obj, user_agent=request.headers.get('user-agent'), ip_address=request.client.host
         )
         session.expunge(url_obj)
 
-    return {
-        'url': url_obj.url
-    }
+    return {'url': url_obj.url}
--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,20 @@ from pydantic import BaseModel, HttpUrl
 
 class UrlRequest(BaseModel):
     url: HttpUrl
+
+
+class UrlResponse(BaseModel):
+    url: HttpUrl
+
+
+class ShortUrlResponse(BaseModel):
+    short_url: str
+
+
+class ErrorSchema(BaseModel):
+    status_code: int
+    message: str
+
+
+class BadRequestSchema(ErrorSchema):
+    detail: str
